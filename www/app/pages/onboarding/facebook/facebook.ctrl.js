6ee8f6eb9f7eb9a6259f2a4c6af77560704a3328
Florian Heiwig <florianheiwig@Florians-MBP.fritz.box>
2016-04-18 23:00:00 +0200

    changed onboarding and etablished the fetching of facebook data

--- www/app/pages/onboarding/facebook/facebook.ctrl.js
+++ www/app/pages/onboarding/facebook/facebook.ctrl.js
@@ -3,4 +3,5 @@ angular.module('app')
   .controller('OnboardingFacebookCtrl', function($rootScope, $scope, $ionicHistory, $state, $account, $stateParams, $window, $facebookLogin){
     $scope.account = $account;
     $scope.facebookLogin = $facebookLogin;
+
   });
--- www/app/pages/onboarding/userdata/userdata.ctrl.js
+++ www/app/pages/onboarding/userdata/userdata.ctrl.js
@@ -1,24 +1,35 @@
 'use strict';
 angular.module('app')
-  .controller('OnboardingUserdataCtrl', function($scope, $rootScope, $state, $stateParams, $window, UsersService){
+  .controller('OnboardingUserdataCtrl', function($scope, $rootScope, $state, $stateParams, $window, $account, UserIdentity, Account){
     $scope.user = {};
     $scope.user = $stateParams.user;
+    $account.load();
+    $scope.user.id = $account.data.id;
+
+    UserIdentity.findById({
+        id: 'me'
+    }, function(identity) {
+        console.log(identity);
+        $scope.user.gender = identity.profile.gender;
+        $scope.user.firstname = identity.profile.name.givenName;
+        $scope.user.lastname = identity.profile.name.familyName;
+        $scope.user.image = identity.profile.photos[0].value;
+    });
+
     $scope.createAccount = function(user){
-      console.log(user);
-      UsersService.signUp(user)
-        .success(function(result){
-          UsersService.login(user)
-            .success(function(result){
-              $window.localStorage['access_token'] = result.id;
-              $window.localStorage['user_id'] = result.userId;
-              $state.go('app.recommendations');
-            })
-            .error(function(result){
-              console.log(result);
-            });
-        })
-        .error(function(result){
-          console.log(result);
+        /*$scope.account = Account.findOne({ id: user.id });
+        $scope.account.firstname = user.firstname;
+        $scope.account.$save();*/
+
+        Account.prototype$updateAttributes({id: user.id},{
+            firstname: user.firstname,
+            lastname: user.lastname,
+            gender: user.gender,
+            image: user.image,
+            city: user.city,
+            moved: user.moved
+        }).$promise.then(function(newUser) {
+            $state.go('app.userevents');
         });
     }
   });
