c786ac95d4e1142ca4b52444a6a49f3dbf677546
Florian Heiwig <florianheiwig@Florians-MacBook-Pro.local>
2016-04-20 16:05:09 +0200

    added logout and some button css definitions

--- www/app/pages/layout/loading.ctrl.js
+++ www/app/pages/layout/loading.ctrl.js
@@ -1,11 +1,15 @@
-
 'use strict';
 angular.module('app')
-  .controller('LoadingCtrl', function($state, $ionicHistory, $window){
-      $ionicHistory.nextViewOptions({
-        disableAnimate: true,
-        disableBack: true
-      });
-      $state.go('onboarding-welcome');
-    }
-  );
+  .controller('LoadingCtrl', function($state, $ionicHistory, $window, $account, Account) {
+    $ionicHistory.nextViewOptions({
+      disableAnimate: true,
+      disableBack: true
+    });
+    Account.findById({
+      id: 'me'
+    }).$promise.then(function(account) {
+      $state.go('app.userevents');
+    }, function(error) {
+        $state.go('onboarding-welcome');
+    });
+  });
